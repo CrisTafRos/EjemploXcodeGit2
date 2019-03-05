c16cee05153da2d9350cc6fae3d67c9fb137b623
Laboratorio UNAM-Apple 14 <laboratoriounam-apple14@macbook-pro-de-laboratorio-14.local>
2019-03-05 14:28:21 -0600

    Initial Commit

---
+++ EjemploXcodeGit2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EjemploXcodeGit2
+//
+//  Created by Laboratorio UNAM-Apple 14 on 05/03/19.
+//  Copyright © 2019 Tafolla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
